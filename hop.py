d10ee356e53d8bc1a255f67afa41b47ecae55d8e
Tennin Yan <tcccc33@gmail.com>
2015-08-09 18:42:18 +0900

    calculate W

--- hop.py
+++ hop.py
@@ -1,7 +1,9 @@
  # -*- coding: utf-8 -*-
 import numpy as np
+# from numpy import linalg as la
 
 data_num = 6
+use_num = 2
 
 train_data = np.array(
         # A
@@ -53,10 +55,22 @@ def print_dot(data):
     print ""
 
 
+def calculate_w(main_data):
+    W = np.zeros((25,25))
+    w_data = train_data.reshape((data_num,25,1))
+    for i in range(use_num):
+        W = W + w_data[i].dot(w_data[i].transpose())
+        # W = (train_data[i].transpose()).dot(train_data[i])
+    for i in range(25):
+        W[i][i] = 0
+    print W
+
+
 
 
 if __name__ == "__main__":
     main_data = train_data.reshape((data_num,5,5))
-    for i in range(data_num):
-        print_dot(main_data[i])
+    # for i in range(data_num):
+    #     print_dot(main_data[i])
+    calculate_w(main_data)
 
